dcd0122a4426dd947c5c1b75777f6d308c3e6e40
Eric <126840543+eccol@users.noreply.github.com>
2024-02-10 15:11:07 -0500

    Add work experience section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import Info from './components/Info.jsx';
 import Schools from './components/Schools.jsx';
+import Jobs from './components/Jobs.jsx';
 
 function App() {
   const [editMode, setEditMode] = useState(true);
@@ -17,6 +18,7 @@ function App() {
         {editMode ? 'Save' : 'Edit'}
       </button>
       <Info editMode={editMode} />
+      <Jobs editMode={editMode} />
       <Schools editMode={editMode} />
     </>
   );
---
+++ src/components/Jobs.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
+export default function Jobs({ editMode }) {
+  const [jobs, setJobs] = useState([]);
+
+  function addJob() {
+    const id = uuid();
+    const newJob = {
+      title: 'Senior Worker',
+      employer: 'Contoso',
+      years: '2010-2015',
+      id: id,
+    };
+    setJobs([...jobs, newJob]);
+  }
+
+  function removeJob(jobID) {
+    const updatedJobs = jobs.filter((job) => job.id !== jobID);
+    setJobs(updatedJobs);
+  }
+
+  function updateJob(jobID, field, value) {
+    const updatedJobs = jobs.map((job) => {
+      if (job.id !== jobID) return job;
+      return { ...job, [field]: value };
+    });
+    setJobs(updatedJobs);
+  }
+
+  const jobList = jobs.map((job) => {
+    return (
+      <div className="job" key={job.id}>
+        <label htmlFor="title">Job Title</label>
+        {editMode ? (
+          <input
+            id="job-title"
+            name="title"
+            value={job.title}
+            onInput={(e) => updateJob(job.id, 'title', e.target.value)}
+          />
+        ) : (
+          <p>{job.name}</p>
+        )}
+        <label htmlFor="employer">Employer</label>
+        {editMode ? (
+          <input
+            id="employer"
+            name="employer"
+            value={job.employer}
+            onInput={(e) => updateJob(job.id, 'employer', e.target.value)}
+          />
+        ) : (
+          <p>{job.employer}</p>
+        )}
+        <label htmlFor="years">Years Worked</label>
+        {editMode ? (
+          <input
+            id="years"
+            name="years"
+            value={job.years}
+            onInput={(e) => updateJob(job.id, 'major', e.target.value)}
+          />
+        ) : (
+          <p>{job.years}</p>
+        )}
+        {editMode ? (
+          <button type="button" onClick={() => removeJob(job.id)}>
+            Remove
+          </button>
+        ) : null}
+      </div>
+    );
+  });
+
+  return (
+    <div className="work-experience">
+      <h1>Work Experience</h1>
+      {jobList}
+      {editMode ? (
+        <button type="button" onClick={addJob}>
+          Add
+        </button>
+      ) : null}
+    </div>
+  );
+}
